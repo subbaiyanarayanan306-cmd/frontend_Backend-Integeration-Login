c0403aefeb8230b5873fd87e94ec5bfe47b50846
subbaiyanarayanan306-cmd <subbaiyanarayanan306@gmail.com>
2026-06-25 18:11:25 +0530

    INITIAL COMMIT FOR FRONTEND AND BACKEND

---
+++ BACKEND/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+const cors = require("cors");
+
+var app = express();
+app.use(cors());
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+var useremail = "subbu@gmail.com";
+var password = 12345;
+app.get("/home", function (req, res) {
+  console.log(req.query.email);
+  console.log(req.query.password);
+  console.log("HOME PAGE RUNNING");
+  // res.send("THIS IS HOME PAGE")
+  if (useremail == req.query.email && password == req.query.password) {
+    res.send(true);
+  }else {
+    res.send(false);
+  }
+});
+
+app.post("/home", function (req, res) {
+  console.log("HOME PAGE RUNNING POST METHOD");
+  res.send("THIS IS HOME PAGE POST METHOD");
+});
+
+app.listen(5000, function () {
+  console.log("SERVER STARTED");
+});
